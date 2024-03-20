7fa333097ac6ec27cac7eb49153cd5708be5e02f
Mattias <mattias@mattiasberg.fi>
2024-03-20 19:45:47 +0200

    feat(car,heatpump cost): purchased electricity cost per consumer is counted based on percentage of total

--- SolarSavings.py
+++ SolarSavings.py
@@ -12,38 +12,24 @@ def _add_value_to_sensor(value, sensor_id):
     state.set(sensor_id, current_value+value)
 
 
-def _calculate_overall_solar_savings_last_hour(last_hour_exported, last_hour_produced, last_hour_buy_price, last_hour_sell_price):
-    overall_savings_last_hour = (last_hour_buy_price * (last_hour_produced - last_hour_exported) + last_hour_sell_price * last_hour_exported)/100.0
-    return overall_savings_last_hour
+def _calculate_overall_solar_savings_last_hour(last_hour_exported_kwh, last_hour_produced_kwh, last_hour_buy_price, last_hour_sell_price):
+    return (last_hour_buy_price * (last_hour_produced_kwh - last_hour_exported_kwh) + last_hour_sell_price * last_hour_exported_kwh)/100.0
 
 
 def _calculate_car_charge_cost_without_solar_last_hour(last_hour_buy_price, last_hour_charged_kwh):
-    car_charge_cost_without_solar_last_hour = (last_hour_buy_price * last_hour_charged_kwh)/100.0
-    return car_charge_cost_without_solar_last_hour
+    return (last_hour_buy_price * last_hour_charged_kwh)/100.0
 
 
-def _calculate_car_charge_cost_with_solar_last_hour(last_hour_buy_price, last_hour_charged_kwh, last_hour_purchased_kwh):
-    car_charge_cost_with_solar_last_hour = 0.0
-    if last_hour_purchased_kwh < 0.001:
-        if last_hour_purchased_kwh < last_hour_charged_kwh:
-            car_charge_cost_with_solar_last_hour = (last_hour_buy_price * (last_hour_charged_kwh - last_hour_purchased_kwh))/100.0
-        else:
-            car_charge_cost_with_solar_last_hour = (last_hour_buy_price * last_hour_charged_kwh)/100.0
-    return car_charge_cost_with_solar_last_hour
+def _calculate_car_charge_cost_with_solar_last_hour(last_hour_buy_price, last_hour_purchased_kwh, car_share_of_purchase):
+    return (last_hour_buy_price * last_hour_purchased_kwh * car_share_of_purchase)/100.0
 
 
 def _calculate_heat_pump_cost_without_solar_last_hour(last_hour_buy_price, last_hour_heat_pump_used_kwh):
     return (last_hour_buy_price * last_hour_heat_pump_used_kwh)/100.0
 
 
-def _calculate_heat_pump_cost_with_solar_last_hour(last_hour_buy_price, last_hour_purchased_kwh, last_hour_heat_pump_used_kwh):
-    heat_pump_cost_with_solar_last_hour = 0.0
-    if last_hour_purchased_kwh < 0.001:
-        if last_hour_purchased_kwh < last_hour_heat_pump_used_kwh:
-            heat_pump_cost_with_solar_last_hour = (last_hour_buy_price * (last_hour_heat_pump_used_kwh - last_hour_purchased_kwh))/100.0
-        else:
-            heat_pump_cost_with_solar_last_hour = (last_hour_buy_price * last_hour_heat_pump_used_kwh)/100.0
-    return heat_pump_cost_with_solar_last_hour
+def _calculate_heat_pump_cost_with_solar_last_hour(last_hour_buy_price, last_hour_purchased_kwh, heat_pump_share_of_purchase):
+    return (last_hour_buy_price * last_hour_purchased_kwh * heat_pump_share_of_purchase)/100.0
 
 
 @time_trigger("cron(2 * * * *)")
@@ -79,28 +65,45 @@ def calculateSolarSavingsLastHour():
     # Fetch all data from history
     last_hour_history = _get_history(last_hour_start, last_hour_end, [exported_kwh_total_entity_id, inverter_yield_kwh_total_entity_id, tesla_wallconnector_energy_entity_id, purchased_kwh_total_entity_id], True, False, False, True)
 
-    # Overall solar savings
-    last_hour_exported = float(last_hour_history[exported_kwh_total_entity_id][-1].state) - float(last_hour_history[exported_kwh_total_entity_id][0].state)
-    last_hour_produced = float(last_hour_history[inverter_yield_kwh_total_entity_id][-1].state) - float(last_hour_history[inverter_yield_kwh_total_entity_id][0].state)
+    # Calculate energy usages last hour
+    last_hour_exported_kwh = float(last_hour_history[exported_kwh_total_entity_id][-1].state) - float(last_hour_history[exported_kwh_total_entity_id][0].state)
+    last_hour_produced_kwh = float(last_hour_history[inverter_yield_kwh_total_entity_id][-1].state) - float(last_hour_history[inverter_yield_kwh_total_entity_id][0].state)
+    last_hour_purchased_kwh = float(last_hour_history[purchased_kwh_total_entity_id][-1].state) - float(last_hour_history[purchased_kwh_total_entity_id][0].state)
+    last_hour_charged_kwh = (float(last_hour_history[tesla_wallconnector_energy_entity_id][-1].state) - float(last_hour_history[tesla_wallconnector_energy_entity_id][0].state))/1000.0
+    last_hour_heat_pump_used_kwh = float(state.get(nibe_energy_used_last_hour_kwh_total_entity_id))
+    last_hour_consumed_solar = last_hour_produced_kwh - last_hour_exported_kwh
 
-    overall_savings_last_hour = _calculate_overall_solar_savings_last_hour(last_hour_exported, last_hour_produced, last_hour_buy_price, last_hour_sell_price)
+    # Correct for kWh purchased exchange for kWh exported during the hour
+    if last_hour_exported_kwh > 0.001 and last_hour_purchased_kwh > 0.001:
+        if last_hour_purchased_kwh >= last_hour_exported_kwh:
+            last_hour_purchased_kwh = last_hour_purchased_kwh - last_hour_exported_kwh
+            last_hour_consumed_solar = last_hour_consumed_solar + last_hour_exported_kwh
+            last_hour_exported_kwh = 0.0
+        else:
+            last_hour_exported_kwh = last_hour_exported_kwh - last_hour_purchased_kwh
+            last_hour_consumed_solar = last_hour_consumed_solar + last_hour_purchased_kwh
+            last_hour_purchased_kwh = 0.0
+
+    car_share_of_purchase = 0.0
+    heat_pump_share_of_purchase = 0.0
+    if last_hour_purchased_kwh > 0.001:
+        # partial purchase with solar
+        # Consumers share solar based on usage
+        car_share_of_purchase = last_hour_charged_kwh / (last_hour_purchased_kwh + last_hour_produced_kwh)
+        heat_pump_share_of_purchase = last_hour_heat_pump_used_kwh / (last_hour_purchased_kwh + last_hour_produced_kwh)
+
+    # Overall solar savings
+    overall_savings_last_hour = _calculate_overall_solar_savings_last_hour(last_hour_exported_kwh, last_hour_produced_kwh, last_hour_buy_price, last_hour_sell_price)
     _add_value_to_sensor(overall_savings_last_hour, solar_savings_entity_id)
 
     # Car charge cost and savings
-    last_hour_charged_kwh = (float(last_hour_history[tesla_wallconnector_energy_entity_id][-1].state) - float(last_hour_history[tesla_wallconnector_energy_entity_id][0].state))/1000.0
-    last_hour_purchased_kwh = float(last_hour_history[purchased_kwh_total_entity_id][-1].state) - float(last_hour_history[purchased_kwh_total_entity_id][0].state)
-
     car_charge_cost_without_solar_last_hour = _calculate_car_charge_cost_without_solar_last_hour(last_hour_buy_price, last_hour_charged_kwh)
-    car_charge_cost_with_solar_last_hour = _calculate_car_charge_cost_with_solar_last_hour(last_hour_buy_price, last_hour_charged_kwh, last_hour_purchased_kwh)
+    car_charge_cost_with_solar_last_hour = _calculate_car_charge_cost_with_solar_last_hour(last_hour_buy_price, last_hour_purchased_kwh, car_share_of_purchase)
     _add_value_to_sensor(car_charge_cost_without_solar_last_hour, car_charge_cost_without_solar_entity_id)
     _add_value_to_sensor(car_charge_cost_with_solar_last_hour, car_charge_cost_with_solar_entity_id)
 
     # Heat pump cost and savings
-    last_hour_heat_pump_used_kwh = float(state.get(nibe_energy_used_last_hour_kwh_total_entity_id))
-
     heat_pump_cost_without_solar_last_hour = _calculate_heat_pump_cost_without_solar_last_hour(last_hour_buy_price, last_hour_heat_pump_used_kwh)
-
-    heat_pump_cost_with_solar_last_hour = _calculate_heat_pump_cost_with_solar_last_hour(last_hour_buy_price, last_hour_purchased_kwh, last_hour_heat_pump_used_kwh)
-
+    heat_pump_cost_with_solar_last_hour = _calculate_heat_pump_cost_with_solar_last_hour(last_hour_buy_price, last_hour_purchased_kwh, heat_pump_share_of_purchase)
     _add_value_to_sensor(heat_pump_cost_without_solar_last_hour, heat_pump_cost_without_solar_entity_id)
     _add_value_to_sensor(heat_pump_cost_with_solar_last_hour, heat_pump_cost_with_solar_entity_id)
