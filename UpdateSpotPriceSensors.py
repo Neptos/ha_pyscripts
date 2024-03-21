8f7968abbe7a519bb2ece0a16b747b13c089f436
Mattias <mattias@mattiasberg.fi>
2024-03-21 08:43:48 +0200

    refactor: removed unused code

--- UpdateSpotPriceSensors.py
+++ UpdateSpotPriceSensors.py
@@ -1,7 +1,6 @@
 from homeassistant.components.recorder import get_instance
-from homeassistant.components.recorder.history import get_significant_states
 from homeassistant.components.recorder.statistics import statistics_during_period
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
 
 
 async def _get_statistic(
@@ -16,21 +15,6 @@ async def _get_statistic(
     #end_time = end_time.astimezone(timezone.utc)
 
     return(await get_instance(hass).async_add_executor_job(statistics_during_period, hass, start_time, end_time, statistic_ids, period, None, types))
-
-
-async def _get_history(
-        start_time: dt,
-        end_time: dt,
-        entity_ids: list[str],
-        include_start_time_state: bool,
-        significant_changes_only: bool,
-        minimal_response: bool,
-        no_attributes: bool):
-
-    start_time = start_time.astimezone(timezone.utc)
-    end_time = end_time.astimezone(timezone.utc)
-
-    return (await get_instance(hass).async_add_executor_job(get_significant_states, hass, start_time, end_time, entity_ids, None, include_start_time_state, significant_changes_only, minimal_response, no_attributes, False))
 
 
 def _get_long_term_prices():
